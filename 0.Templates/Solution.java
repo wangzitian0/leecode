239b7758441a8df3572d5766a2449d121b7dd9e6
wangzitian0 <wangzitian0@gmail.com>
2015-07-19 05:57:30 +0800

    finish 8

---
+++ 0.Templates/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int myAtoi(String str) {
+        int i;
+    }
+    public static void main(String args[])
+    {
+        Solution s=new Solution();
+        String a=" 010";
+        System.out.println(s.myAtoi(a));
+    }
+}
