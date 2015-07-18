f805be9ee566d3d1064b19e0c89715525992b50e
wangzitian0 <wangzitian0@gmail.com>
2015-07-19 06:12:45 +0800

    finish 9

---
+++ Palindrome Number/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        String str=String.valueOf(x);
+        int len=str.length();
+        for(int i=0;i<len;i++)
+        {
+			
+		}
+    }
+    public static void main(String args[])
+    {
+        Solution s=new Solution();
+        String a=" 010";
+        System.out.println(s.myAtoi(a));
+    }
+}
